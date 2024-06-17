7e0e5b76ed6b456b60d0dd5eb2ab1dc0ce5af309
aine-mbabazi <patainembabazi@gmail.com>
2024-06-17 17:23:38 +0300

    Added Attributes

---
+++ course/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+      course_name= models.CharField(max_length =20)
+      course_id = models.PositiveSmallIntegerField()
+      department = models.CharField(max_length=20)
+      course_description = models.TextField()
+      class_hours = models.DurationField()
+      course_instructor = models.CharField(max_length =20)
+      assessment_requirements = models.TextField()
+      course_capacity = models.PositiveSmallIntegerField()
+      grade_level = models.CharField(max_length=15)
+      school_term = models.PositiveSmallIntegerField()
+
+    
+      def __str__(self):
+        return f"{self.course_name} {self.course_id}"
+
---
+++ teacher/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Teacher(models.Model):
+      first_name = models.CharField(max_length =20)
+      last_name = models.CharField(max_length=20)
+      gender = models.CharField(max_length=20)
+      nationality= models.CharField(max_length=20)
+      email = models.EmailField()
+      date_of_joining = models.DateField()
+      teacher_id= models.PositiveSmallIntegerField()
+      course = models.CharField(max_length=20)
+      department = models.CharField(max_length=20)
+      bank_account_number = models.CharField(max_length = 16)
+      
+
+    
+      def __str__(self):
+        return f"{self.first_name} {self.last_name}"
